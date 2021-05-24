3986f158776b9908f7705d27c21bea7791248573
Victor Lupascu <sinajeen@gmail.com>
2021-05-24 18:35:13 -0500

    cancel default event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
-var buttonE1 = document.querySelector('#save-task');
-console.log(buttonE1);
+// var buttonE1 = document.querySelector('#save-task');
+// console.log(buttonE1);
+var formE1 = document.querySelector('#task-form');
 var tasksToDoE1 = document.querySelector('#tasks-to-do');
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+
     var listItemE1 = document.createElement('li');
     listItemE1.className = 'task-item';
     listItemE1.textContent = 'This is a new task.';
     tasksToDoE1.appendChild(listItemE1);
+    // console.log(event);
 };
 
-buttonE1.addEventListener('click', createTaskHandler);
+// buttonE1.addEventListener('click', createTaskHandler);
+formE1.addEventListener('submit', createTaskHandler);

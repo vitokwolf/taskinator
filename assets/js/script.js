e017fc2459aa610a30df9017fe58982af7c66c35
Victor Lupascu <sinajeen@gmail.com>
2021-05-24 19:18:37 -0500

    refractor code to create new div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// var buttonE1 = document.querySelector('#save-task');
 // console.log(buttonE1);
 var formE1 = document.querySelector('#task-form');
 var tasksToDoE1 = document.querySelector('#tasks-to-do');
@@ -6,13 +5,28 @@ var tasksToDoE1 = document.querySelector('#tasks-to-do');
 var createTaskHandler = function (event) {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    // console.dir(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // create list item
     var listItemE1 = document.createElement('li');
     listItemE1.className = 'task-item';
-    listItemE1.textContent = 'This is a new task.';
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement('div');
+    // give it a class name
+    taskInfoEl.className = 'task-info';
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" +
+        taskNameInput + "</h3><spane class='task-type'>" +
+        taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
     // console.log(event);
 };
 
-// buttonE1.addEventListener('click', createTaskHandler);
 formE1.addEventListener('submit', createTaskHandler);
